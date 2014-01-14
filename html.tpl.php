c9af253e770d341e4ef585683219ebff53b2dd9d
David Norman <deekayen@deekayen.net>
2014-01-14 17:27:53 -0500

    Move the Javascript loading later on the page

--- html.tpl.php
+++ html.tpl.php
@@ -33,18 +33,19 @@ if(!theme_get_setting('mothership_script_place_footer')) {
       <?php print $page_top; //stuff from modules always render first ?>
       <?php print $page; // uses the page.tpl ?>
 
-      <?php
-        if(theme_get_setting('mothership_script_place_footer')) {
-          print $scripts;
-        }
-      ?>
-
       <footer class="vcard">
         <a href="https://www.linkedin.com/in/deekayen" class="footer-1"><i class="fa fa-linkedin"></i> <span class="label">Profile</span></a>
         <a href="mai&#108;&#116;o&#58;%&#54;4&#101;e&#107;%6&#49;&#121;e&#110;&#64;d&#101;ekay&#37;&#54;5n&#46;&#110;et" class="footer-2"><i class="fa fa-envelope-o"></i> <span class="label email">dee&#107;aye&#110;&#64;deeka&#121;en&#46;ne&#116;</span></a>
         <a href="http://twitter.com/davidknorman" class="footer-3"><i class="fa fa-twitter"></i> <span class="label">@davidknorman</span></a>
         <a href="https://github.com/deekayen" class="footer-4"><i class="fa fa-github"></i> <span class="label">Github</span></a>
       </footer>
+
+      <?php
+        if(theme_get_setting('mothership_script_place_footer')) {
+          print $scripts;
+        }
+      ?>
+
       <?php print $page_bottom; //stuff from modules always render last ?>
     </body>
   </html>
