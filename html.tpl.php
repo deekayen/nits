5039d316852381648c94e438dc15c828b02f8c1e
David Norman <deekayen@deekayen.net>
2014-08-26 14:41:58 -0400

    Replace LinkedIn and with RSS feeds.
    Unsupport Apple touch icons.

--- html.tpl.php
+++ html.tpl.php
@@ -5,11 +5,6 @@
       <meta charset="utf-8">
       <title><?php print $head_title; ?></title>
       <?php print str_replace("\0", '', $head); ?>
-      <link rel="apple-touch-icon" href="<?php print drupal_get_path('theme', 'nits'); ?>/apple-touch-icon.png"/>
-      <link rel="apple-touch-icon-precomposed" href="<?php print drupal_get_path('theme', 'nits'); ?>/apple-touch-icon-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php print drupal_get_path('theme', 'nits'); ?>/apple-touch-icon-72x72-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php print drupal_get_path('theme', 'nits'); ?>/apple-touch-icon-114x114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php print drupal_get_path('theme', 'nits'); ?>/apple-touch-icon-144x144-precomposed.png">
       <meta name="HandheldFriendly" content="True">
       <meta name="MobileOptimized" content="320">
 <?php if(theme_get_setting('mothership_viewport')){  ?>
@@ -33,19 +28,18 @@ if(!theme_get_setting('mothership_script_place_footer')) {
       <?php print $page_top; //stuff from modules always render first ?>
       <?php print $page; // uses the page.tpl ?>
 
-      <footer class="vcard">
-        <a href="https://www.linkedin.com/in/deekayen" class="footer-1"><i class="fa fa-linkedin"></i> <span class="label">Profile</span></a>
-        <a href="mai&#108;&#116;o&#58;%&#54;4&#101;e&#107;%6&#49;&#121;e&#110;&#64;d&#101;ekay&#37;&#54;5n&#46;&#110;et" class="footer-2"><i class="fa fa-envelope-o"></i> <span class="label email">dee&#107;aye&#110;&#64;deeka&#121;en&#46;ne&#116;</span></a>
-        <a href="http://twitter.com/davidknorman" class="footer-3"><i class="fa fa-twitter"></i> <span class="label">@davidknorman</span></a>
-        <a href="https://github.com/deekayen" class="footer-4"><i class="fa fa-github"></i> <span class="label">Github</span></a>
-      </footer>
-
       <?php
         if(theme_get_setting('mothership_script_place_footer')) {
           print $scripts;
         }
       ?>
 
+      <footer class="vcard">
+        <a href="https://deekayen.net/rss.xml" class="footer-1"><i class="fa fa-rss"></i> <span class="label">RSS feed</span></a>
+        <a href="mai&#108;&#116;o&#58;%&#54;4&#101;e&#107;%6&#49;&#121;e&#110;&#64;d&#101;ekay&#37;&#54;5n&#46;&#110;et" class="footer-2"><i class="fa fa-envelope-o"></i> <span class="label email">dee&#107;aye&#110;&#64;deeka&#121;en&#46;ne&#116;</span></a>
+        <a href="https://www.drupal.org/u/deekayen" class="footer-3"><i class="fa fa-drupal"></i> <span class="label">Drupal profile</span></a>
+        <a href="https://github.com/deekayen" class="footer-4"><i class="fa fa-github"></i> <span class="label">GitHub profile</span></a>
+      </footer>
       <?php print $page_bottom; //stuff from modules always render last ?>
     </body>
   </html>
