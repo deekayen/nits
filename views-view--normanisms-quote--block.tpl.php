a810b47be628e4000b87f5d1910297439dccb3e7
David Norman <deekayen@deekayen.net>
2014-08-26 14:41:06 -0400

    Don't show people a link to submit Normanisms.

--- views-view--normanisms-quote--block.tpl.php
+++ views-view--normanisms-quote--block.tpl.php
@@ -2,7 +2,7 @@
 <div class="<?php print $classes; ?>">
   <?php if ($rows): ?>
     <aside>
-      <p><cite>"<?php print trim(strip_tags($rows, "<a>")); ?>"</cite> <small><a href="<?php print url('node/add/normanism'); ?>" style="display: inline" class="fa fa-pencil" title="Submit a Normanism"></a> <a href="<?php print url('normanisms.xml'); ?>" style="display: inline" class="fa fa-rss" title="Normanisms feed"></a></small></p>
+      <p><cite>"<?php print trim(strip_tags($rows, "<a>")); ?>"</cite> <small><a href="<?php print url('normanisms.xml'); ?>" style="display: inline" class="fa fa-rss" title="Normanisms feed"></a></small></p>
     </aside>
   <?php elseif ($empty): ?>
     <div class="view-empty">
